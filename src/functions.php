3a49f995f4bb8fdec351b32082f69c8d6994332e
Bence Agócs <bence@harom.studio>
2021-09-23 17:33:18 +0200

    products page title moved before loop

--- src/functions.php
+++ src/functions.php
@@ -31,6 +31,6 @@ function mytheme_add_woocommerce_support() {
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
-// add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 5 );
+add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_title', 5 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs');
