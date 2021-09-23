760c5fbea6b970c4cb7718510e8aa9950c952c64
Bence Agócs <bence@harom.studio>
2021-09-23 16:32:24 +0200

    products tab removed

--- dist/functions.php
+++ dist/functions.php
@@ -30,3 +30,5 @@ function mytheme_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs');
