021b8932bc325b63ce6aac1079815c8373e91a89
Bence Agócs <bence@harom.studio>
2021-09-28 21:17:35 +0200

    update search path

--- src/search.php
+++ src/search.php
@@ -10,9 +10,19 @@
     if($type == 'post') {
         echo '<h1>Bejegyzések a találatban</h1>';
     } elseif($type == 'product') {
-        // load_template(TEMPLATEPATH . '/knowledge-search.php');
-        echo '<h1>Termékek a találatban</h1>';
-    }
+        // load_template(TEMPLATEPATH . '/knowledge-search.php'); ?>
+        <div class="container grid grid-4">
+            <h1>Termékek</h1>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php global $product; ?>
+                <a href="<?php echo get_permalink(); ?>" class="product-tile mb-1">
+                    <?php echo woocommerce_get_product_thumbnail(); ?>
+                    <h2 class="product-name"><?php echo get_the_title(); ?></h2>
+                    <p class="product-price"><?php $product->get_price(); ?></p>
+                </a>
+            <?php endwhile; ?>
+        </div>    
+    <? }
 } else { ?>
 	<div class="container grid grid-4">
         <?php while ( have_posts() ) : the_post(); ?>
