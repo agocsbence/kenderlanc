fba6e2c8dff2476b507ac83292dda131fff253f5
Bence Agócs <bence@harom.studio>
2021-09-29 01:53:43 +0200

    add services

--- src/single-szolgaltatas.php
+++ src/single-szolgaltatas.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <h1 class="mb-2"><?php the_title(); ?></h1>
-    <div class="grid grid-2">
+    <div class="grid grid-2 grid-gap-2">
         <div class="text-block">
             <?php the_content(); ?>
         </div>
