334de15a38f44412046fac02be447f68919e6a10
Bence Agócs <bence@harom.studio>
2021-09-28 22:22:08 +0200

    desktop menu toggler

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,12 +1,20 @@
 //toggle menu opening
 var mobileMenuToggle = document.querySelector('#mobileMenuToggler');
 var mobileMenuClose = document.querySelector('#mobileMenuClose');
+var desktopMenuToggle = document.querySelector('#desktopMenuToggler');
+var desktopMenuClose = document.querySelector('#desktopMenuClose');
 var navbar = document.querySelector('#navbar');
 
 mobileMenuToggle.addEventListener('click', function() {
     navbar.classList.add('opened');
 });
 mobileMenuClose.addEventListener('click', function() {
+    navbar.classList.remove('opened');
+});
+desktopMenuToggle.addEventListener('click', function() {
+    navbar.classList.add('opened');
+});
+desktopMenuClose.addEventListener('click', function() {
     navbar.classList.remove('opened');
 });
 
