f725e6adbe42fa6ebbadfe7f0c3488c1c4e9d994
Bence Agócs <bence@harom.studio>
2021-09-28 21:38:41 +0200

    update search

--- dist/search.php
+++ dist/search.php
@@ -1,50 +1,42 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying Search Results pages.
+ */
+
+get_header(); ?>
 
 <section class="container py-3">
     <h1 class="mb-1">Keresés</h1>
 </section>
 
-<?php if(isset($_GET['search-type'])) {
-    $type = $_GET['search-type'];
-    var_dump($type);
-    if($type == 'post') {
-        echo '<h1>Bejegyzések a találatban</h1>';
-    } elseif($type == 'product') {
-        // load_template(TEMPLATEPATH . '/knowledge-search.php'); ?>
-        <div class="container grid grid-4">
-            <h1>Termékek</h1>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php global $product; ?>
-                <a href="<?php echo get_permalink(); ?>" class="product-tile mb-1">
-                    <?php echo woocommerce_get_product_thumbnail(); ?>
-                    <h2 class="product-name"><?php echo get_the_title(); ?></h2>
-                    <p class="product-price"><?php $product->get_price(); ?></p>
-                </a>
-            <?php endwhile; ?>
-        </div>    
-    <? } else { ?>
-        <div class="container grid grid-4">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php global $product; ?>
-                <a href="<?php echo get_permalink(); ?>" class="product-tile mb-1">
-                    <?php echo woocommerce_get_product_thumbnail(); ?>
-                    <h2 class="product-name"><?php echo get_the_title(); ?></h2>
-                    <p class="product-price"><?php $product->get_price(); ?></p>
-                </a>
-            <?php endwhile; ?>
-        </div>
-    <?php }
-} else { ?>
-	<div class="container grid grid-4">
+<section id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+
+    <?php if ( have_posts() ) : ?>
+
+        <header class="page-header">
+            <h1 class="page-title"><?php printf( __( 'Keresés: %s', 'shape' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+        </header><!-- .page-header -->
+
+        <?php /* Start the Loop */ ?>
         <?php while ( have_posts() ) : the_post(); ?>
+
             <?php global $product; ?>
             <a href="<?php echo get_permalink(); ?>" class="product-tile mb-1">
                 <?php echo woocommerce_get_product_thumbnail(); ?>
                 <h2 class="product-name"><?php echo get_the_title(); ?></h2>
                 <p class="product-price"><?php $product->get_price(); ?></p>
             </a>
+
         <?php endwhile; ?>
-    </div>
-<?php } ?>
+
+    <?php else : ?>
+
+        <h1>A keresés nem hozott eredményt :(</h1>
+
+    <?php endif; ?>
+
+    </div><!-- #content .site-content -->
+</section><!-- #primary .content-area -->
 
 <?php get_footer(); ?>
