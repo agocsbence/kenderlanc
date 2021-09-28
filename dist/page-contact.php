4b9b2653540ca1f4a88d359ef541dc4937229cf5
Bence Agócs <bence@harom.studio>
2021-09-28 23:36:30 +0200

    style update

---
+++ dist/page-contact.php
@@ -0,0 +1,22 @@
+<?php
+/* Template Name: Kapcsolat */
+get_header();
+?>
+
+<section class="container py-3 single-text-content">
+    <h1 class="mb-1"><?php the_title(); ?></h1>
+    <div class="grid grid-2 grid-gap-2">
+        <div class="text-left">
+            <p class="strong">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ullam temporibus exercitationem laborum eos vitae porro? Tempora, fugit in tenetur neque eveniet, error est modi minus alias distinctio ducimus ipsum.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ullam temporibus exercitationem laborum eos vitae porro? Tempora, fugit in tenetur neque eveniet, error est modi minus alias distinctio ducimus ipsum.</p>
+        </div>
+        <div class="text-right">
+            <p class="strong">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ullam temporibus exercitationem laborum eos vitae porro? Tempora, fugit in tenetur neque eveniet, error est modi minus alias distinctio ducimus ipsum.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ullam temporibus exercitationem laborum eos vitae porro? Tempora, fugit in tenetur neque eveniet, error est modi minus alias distinctio ducimus ipsum.</p>
+            <p class="strong">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ullam temporibus exercitationem laborum eos vitae porro? Tempora, fugit in tenetur neque eveniet, error est modi minus alias distinctio ducimus ipsum.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ullam temporibus exercitationem laborum eos vitae porro? Tempora, fugit in tenetur neque eveniet, error est modi minus alias distinctio ducimus ipsum.</p>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
