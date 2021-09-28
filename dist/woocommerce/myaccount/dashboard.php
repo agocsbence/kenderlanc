2d1a3e1b42087f0718e6d9ed8762e50ff954d852
Bence Agócs <bence@harom.studio>
2021-09-29 00:35:42 +0200

    style update

--- dist/woocommerce/myaccount/dashboard.php
+++ dist/woocommerce/myaccount/dashboard.php
@@ -28,18 +28,19 @@ $allowed_html = array(
 );
 ?>
 
-<p>
+<h3>
 	<?php
 	printf(
 		/* translators: 1: user display name 2: logout url */
-		wp_kses( __( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ), $allowed_html ),
+		// wp_kses( __( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ), $allowed_html ),
+        wp_kses( __( 'Hello %1$s', 'woocommerce' ), $allowed_html ),
 		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
 		esc_url( wc_logout_url() )
 	);
 	?>
-</p>
+</h3>
 
-<p>
+<p class="font-300">
 	<?php
 	/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
 	$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
