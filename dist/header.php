2cc836d850c2ebdb5f064dfa83245d7063343899
Bence Agócs <bence@harom.studio>
2021-09-28 21:13:22 +0200

    update search path

--- dist/header.php
+++ dist/header.php
@@ -50,6 +50,8 @@
         <div class="header-search">
             <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
                 <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Keresés', 'placeholder', 'kenderlanc' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+                <input type="hidden" name="post_type[]" value="product" />
+                <input type="hidden" name="post_type[]" value="post" />
                 <!-- <button type="submit" class="search-submit btn">Keresés</button> -->
             </form>
         </div>
