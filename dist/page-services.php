7be694034b7e13b5a58d4997a86f9605942139b0
Bence Agócs <bence@harom.studio>
2021-09-29 01:35:51 +0200

    add services

--- dist/page-services.php
+++ dist/page-services.php
@@ -7,22 +7,22 @@ get_header(); ?>
 <div class="container">
     <h1 class="mb-2">Szolgáltatások</h1>
     <div class="grid grid--home-tiles">
-        <a href="" class="grid-tile big-tile title-top bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/szolgaltatasok.png');">
+        <a href="<?php echo get_home_url(); ?>/szolgaltatas/vetomag-termesztes" class="grid-tile big-tile title-top bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/vetomag.png');">
             <h2>Vetőmag termesztés</h2>
         </a>
-        <a href="" id="jogsegely" class="grid-tile bg--color bg--color-red">
+        <a href="<?php echo get_home_url(); ?>/szolgaltatas/jogsegely" id="jogsegely" class="grid-tile bg--color bg--color-red">
             <h2>Jogsegély</h2>
         </a>
-        <a href="/termekek" class="grid-tile bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/nepszeru_termekek.png');">
+        <a href="/termekek" class="grid-tile bg--color bg--color-green">
             <h2>Magtól a termékig</h2>
         </a>
-        <a href="" class="grid-tile bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/partnereink.png');">
+        <a href="" class="grid-tile bg--color bg--color-green1">
             <h2>Oktatás</h2>
         </a>
-        <a href="" class="grid-tile bg--color bg--color-green">
+        <a href="" class="grid-tile bg--color bg--color-orange">
             <h2>Forgalmazás</h2>
         </a>
-        <a href="" class="grid-tile bg--color bg--color-orange">
+        <a href="" class="grid-tile bg--color bg--color-green">
             <h2>Rendezvény</h2>
         </a>
     </div>
