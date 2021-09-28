210d0bbe683c8b1b34e5bba69ac0d76ffe691463
Bence Agócs <bence@harom.studio>
2021-09-28 11:07:11 +0200

    update single product site

--- src/header.php
+++ src/header.php
@@ -25,7 +25,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/style.css?v=1.0.0">
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/style.css?v=1.0.1">
 </head>
 
 <body <?php body_class(); ?>>
