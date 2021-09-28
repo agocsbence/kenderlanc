facbf5b4a6f989ccb384b4112f82125b715c7650
Bence Agócs <bence@harom.studio>
2021-09-28 12:21:35 +0200

    update single page

--- src/functions.php
+++ src/functions.php
@@ -44,3 +44,5 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 10);
 //remove price hook (added directly to thumbnails hook)
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+//remove product meta
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
