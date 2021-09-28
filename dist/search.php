b5192e77b928e1d8a7eda24fb8d5bf9d90e72bee
Bence Agócs <bence@harom.studio>
2021-09-28 21:10:19 +0200

    update search path

--- dist/search.php
+++ dist/search.php
@@ -13,11 +13,11 @@
         echo '<h1>Termékek a találatban</h1>';
     }
 } else { ?>
-	<div class="container grid grid-3">
+	<div class="container grid grid-4">
         <?php while ( have_posts() ) : the_post(); ?>
             <?php global $product; ?>
-            <a href="<?php echo get_permalink(); ?>" class="product-tile">
-                <img src="<?php echo woocommerce_get_product_thumbnail(); ?>" alt="Termék neve">
+            <a href="<?php echo get_permalink(); ?>" class="product-tile mb-1">
+                <?php echo woocommerce_get_product_thumbnail(); ?>
                 <h2 class="product-name"><?php echo get_the_title(); ?></h2>
                 <p class="product-price"><?php $product->get_price(); ?></p>
             </a>
