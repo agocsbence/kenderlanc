20480c9e9e8be3166397dc4fd605cdd3e1654345
Bence Agócs <bence@harom.studio>
2021-09-28 21:21:50 +0200

    update search path

--- src/search.php
+++ src/search.php
@@ -23,7 +23,16 @@
             <?php endwhile; ?>
         </div>    
     <? } else { ?>
-        <h1>Itt vagyok</h1>
+        <div class="container grid grid-4">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php global $product; ?>
+                <a href="<?php echo get_permalink(); ?>" class="product-tile mb-1">
+                    <?php echo woocommerce_get_product_thumbnail(); ?>
+                    <h2 class="product-name"><?php echo get_the_title(); ?></h2>
+                    <p class="product-price"><?php $product->get_price(); ?></p>
+                </a>
+            <?php endwhile; ?>
+        </div>
     <?php }
 } else { ?>
 	<div class="container grid grid-4">
