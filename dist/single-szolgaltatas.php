31355ff70534ace2749c5abf353978be718d2534
Bence Agócs <bence@harom.studio>
2021-09-29 01:42:00 +0200

    add services

---
+++ dist/single-szolgaltatas.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <h1 class="mb-2"><?php the_title(); ?></h1>
+    <div class="grid grid-2">
+        <div class="text-block">
+            <?php the_content(); ?>
+        </div>
+        <div class="text-block form-block">
+            <?php $form = get_field('form'); ?>
+            <?php echo do_shortcode($form); ?>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();
