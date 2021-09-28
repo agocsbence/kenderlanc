bb037391772e6f13f9dffe91b9da9c794d1fbb80
Bence Agócs <bence@harom.studio>
2021-09-29 01:28:19 +0200

    add services

--- dist/page-services.php
+++ dist/page-services.php
@@ -3,23 +3,26 @@
 /* Template Name: Szolgáltatások */
 
 get_header(); ?>
-<?php
-    wp_reset_postdata();
-    $services_query = new WP_Query(array(
-        'post_type' => 'szolgaltatas',
-    ));
-?>
-    <?php if ($services_query->have_posts()) { ?>
 
-        <div class="services-grid grid grid-3">
-            <?php while ($services_query->have_posts()) { ?>
-                <?php $services_query->the_post(); 
-                the_title();
-            } ?>
-        </div>
-
-    <?php wp_reset_postdata(); ?>
-
-    <?php } ?>
+<div class="grid grid--home-tiles">
+    <a href="" class="grid-tile big-tile title-top bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/szolgaltatasok.png');">
+        <h2>Vetőmag termesztés</h2>
+    </a>
+    <a href="" id="jogsegely" class="grid-tile bg--color bg--color-red">
+        <h2>Jogsegély</h2>
+    </a>
+    <a href="/termekek" class="grid-tile bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/nepszeru_termekek.png');">
+        <h2>Magtól a termékig</h2>
+    </a>
+    <a href="" class="grid-tile bg--image" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/partnereink.png');">
+        <h2>Oktatás</h2>
+    </a>
+    <a href="" class="grid-tile bg--color bg--color-green">
+        <h2>Forgalmazás</h2>
+    </a>
+    <a href="" class="grid-tile bg--color bg--color-orange">
+        <h2>Rendezvény</h2>
+    </a>
+</div>
 
 <?php get_footer();
