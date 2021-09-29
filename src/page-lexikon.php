9fe62f2db2bb8c9e0cd32f55faa2e24685171132
Bence Agócs <bence@harom.studio>
2021-09-29 02:48:06 +0200

    add services

---
+++ src/page-lexikon.php
@@ -0,0 +1,45 @@
+<?php
+
+/* Template Name: Lexikon */
+
+get_header(); ?>
+
+<div class="container">
+    <h1 class="mb-2">Lexikon</h1>
+    <div class="lexikon-wrapper">
+        <div class="lexikon-alphabet">
+            <div class="alphabet-letters">
+                <a href="#a">A</a>
+                <a href="#b">B</a>
+                <a href="">C</a>
+                <a href="">D</a>
+                <a href="">E</a>
+                <a href="">F</a>
+                <a href="">G</a>
+            </div>
+        </div>
+        <div class="lexikon">
+            <div class="letter">
+                <h2 id="a">A</h2>
+                <div class="lexikon-article">
+                    <h3>Áztatás (Soak)</h3>
+                    <p>A kenderkóróknak – a víz hőmérsékletétől függően – 7–14 napon át víz alatt való tartása, hogy a szár külsején elhelyezkedő értékes rostanyag lelazuljon a kender belső fás szövetéről. A kenderáztatás mind álló-, mind folyóvízben
+                        szokásos, bár a nagyobb folyóvizekben való áztatást tiltották a hatóságok, mivel a kenderből kioldódó anyagok mérgezték a halállományt.</p>
+                </div>
+            </div>
+            <div class="letter">
+                <h2 id="b">B</h2>
+                <div class="lexikon-article">
+                    <h3>Bakol</h3>
+                    <p>Régies kifejezés. Kendert tilol, tör.</p>
+                </div>
+                <div class="lexikon-article">
+                    <h3>Bakóallya</h3>
+                    <p>Régies kifejezés. Az a csomó kender, amelyet egyszerre tesznek a törő alá</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();
