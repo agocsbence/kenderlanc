20ba6ced26f34f740c5ee8b8cfff0aa5902c9f90
Bence Agócs <bence@harom.studio>
2021-09-29 03:26:11 +0200

    add filter to accordion

--- dist/page-akademia.php
+++ dist/page-akademia.php
@@ -32,6 +32,47 @@ get_header(); ?>
         </form>
     </div>
     <div class="accordion-wrapper">
+
+        <!-- loop here -->
+        <h2>tagek:</h2>
+        <?php
+        $args = array(
+            'type' => get_post_type(),
+            'orderby' => 'name',
+            'order' => 'ASC'
+        );
+        $tags = get_tags($args);
+        
+        foreach($tags as $tag) { 
+            var_dump($tag->name);
+        }
+        
+        echo "<hr>";
+        
+        $loop = new WP_Query( array( 'post_type' => 'akademia_cikkek', 'tag' => 'current', 'ignore_sticky_posts' => 1, 'paged' => $paged ) );
+        if ( $loop->have_posts() ) :
+            while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                <div class="pindex">
+                    <?php if ( has_post_thumbnail() ) { ?>
+                        <div class="pimage">
+                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+                        </div>
+                    <?php } ?>
+                    <div class="ptitle">
+                        <h2><?php echo get_the_title(); ?></h2>
+                    </div>
+                </div>
+            <?php endwhile;
+            if (  $loop->max_num_pages > 1 ) : ?>
+                <div id="nav-below" class="navigation">
+                    <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Previous', 'domain' ) ); ?></div>
+                    <div class="nav-next"><?php previous_posts_link( __( 'Next <span class="meta-nav">&rarr;</span>', 'domain' ) ); ?></div>
+                </div>
+            <?php endif;
+        endif;
+        wp_reset_postdata();
+    ?>
+
         <div class="accordion">
             <div class="accordion-header">
                 <h1>Életvirág szappan</h1>
@@ -51,6 +92,9 @@ get_header(); ?>
                 <p><strong>vilhemp.com</strong></p>
             </div>
         </div>
+
+        <!-- end of loop -->
+
         <div class="accordion">
             <div class="accordion-header">
                 <h1>Életvirág szappan</h1>
