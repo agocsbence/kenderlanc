4a77933e059103c0d9f0f83f3c4d9e7f2c3af339
Bence Agócs <bence@harom.studio>
2021-09-30 07:20:02 +0200

    update header to avoid scroll anim

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -19,13 +19,13 @@ desktopMenuClose.addEventListener('click', function() {
 });
 
 //show/hide navbar on scroll back
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("header").style.top = "0";
-    } else {
-        document.getElementById("header").style.top = "-9rem";
-    }
-    prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("header").style.top = "0";
+//     } else {
+//         document.getElementById("header").style.top = "-9rem";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
