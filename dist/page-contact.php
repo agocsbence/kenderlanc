a4482e962e92260c494d1fcd48f987c0239afd2c
Bence Agócs <bence@harom.studio>
2021-10-15 15:47:23 +0200

    update contact page

--- dist/page-contact.php
+++ dist/page-contact.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 
-<section class="container py-3 single-text-content">
+<section class="container single-text-content">
     <h1 class="mb-3"><?php the_title(); ?></h1>
     <div class="grid grid-2 grid-gap-2">
         <div class="text-block">
