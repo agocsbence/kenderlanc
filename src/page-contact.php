c65aee54ea2dfef59efa60cf0843710170e4d0bb
Bence Agócs <bence@harom.studio>
2021-10-17 18:09:03 +0200

    update style on login form rememberme input

--- src/page-contact.php
+++ src/page-contact.php
@@ -5,7 +5,7 @@ get_header();
 
 <section class="container single-text-content">
     <h1 class="mb-3"><?php the_title(); ?></h1>
-    <div class="grid grid-2 grid-gap-2">
+    <div class="grid grid-2">
         <div class="text-block">
             <?php the_field('bal_hasab'); ?>
         </div>
