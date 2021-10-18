68e623412dae4c49d0c5eb3a1c66ee1c5382b06d
Bence Agócs <bence@harom.studio>
2021-10-18 21:53:23 +0200

    add rolunk page template

---
+++ src/page-rolunk.php
@@ -0,0 +1,70 @@
+<?php
+
+/* Template Name: Rólunk */
+
+get_header(); ?>
+
+<div class="container">
+    <h1 class="mb-2">Kenderlánc</h1>
+    <div class="rolunk-lead">
+        <p>Ha kendered van, mindened van!”<br>Célunk, hogy ezt a méltatlanul elfeledett, évezredes hagyományokkal rendelkező kultúr- és haszonnövényt visszaemeljük a megérdemelt helyére a hazai gazdaságba és a benne rejlő lehetőségeket felhasználva, közösen építsünk egy egészségesebb, környezettudatosabb, zöld társadalmat.</p>
+        <p>Azért dolgozunk, hogy a természettel összhangban elősegítsük az ipari kender termesztését, feldolgozását és az ebből készült változatos termékek elérhetőségét a mindennapokban.</p>
+    </div>
+    <div class="team-wrapper">
+        <div class="team-member">
+            <h2>Vincze János (26)</h2>
+            <img src="" alt="">
+            <div class="member-description">
+                <p>Gyermekként (is) a természet volt a legjobb barátom. Nagyapám foglalkozása gulyás volt, nyaranta sok időt töltöttem vele a legelőkön és sokat tanított a természettel való összhangról. Felmenőim között csupa parasztemberek foglalnak helyet, így nem is volt soha kérdéses, hogy mivel szeretnék foglalkozni.</p>
+                <p>Pár évvel ezelőtt ez az álom kezdett egyre jobban kikristályosodni, ugyanis egy nagyszerű közösséghez csatlakozva, útjára indítottuk a Zengető fesztivált.  Már akkor mélyebben belevetettem magam az ipari kender rejtelmeibe és próbáltam a lehető legtöbb tudást magamhoz venni és megismerkedni még jobban ezzel a haszonnövénnyel.</p>
+                <p>Rendezvényszervező szakmámnak köszönhetően sok hazai mezőgazdásszal, termelővel és szakemberrel tartom a kapcsolatot, illetve jómagam is elkezdtem mezőgazdasággal foglalkozni. Feleségemmel közösen épp egy 5 hektáros tanyára költözünk, beteljesítve ezzel végleges elköteleződésünket a természet és az ipari kender felé.</p>
+                <p>A Kenderlánc egyik alapítójaként az a célom, hogy fellendítsük a hazai ipari kender termesztését, segítsük  a mezőgazdaságot, és hosszú távon, közös együttműködésben erősítsük a növény útját.</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <h2>Vincze János (26)</h2>
+            <img src="" alt="">
+            <div class="member-description">
+                <p>Gyermekként (is) a természet volt a legjobb barátom. Nagyapám foglalkozása gulyás volt, nyaranta sok időt töltöttem vele a legelőkön és sokat tanított a természettel való összhangról. Felmenőim között csupa parasztemberek foglalnak helyet, így nem is volt soha kérdéses, hogy mivel szeretnék foglalkozni.</p>
+                <p>Pár évvel ezelőtt ez az álom kezdett egyre jobban kikristályosodni, ugyanis egy nagyszerű közösséghez csatlakozva, útjára indítottuk a Zengető fesztivált.  Már akkor mélyebben belevetettem magam az ipari kender rejtelmeibe és próbáltam a lehető legtöbb tudást magamhoz venni és megismerkedni még jobban ezzel a haszonnövénnyel.</p>
+                <p>Rendezvényszervező szakmámnak köszönhetően sok hazai mezőgazdásszal, termelővel és szakemberrel tartom a kapcsolatot, illetve jómagam is elkezdtem mezőgazdasággal foglalkozni. Feleségemmel közösen épp egy 5 hektáros tanyára költözünk, beteljesítve ezzel végleges elköteleződésünket a természet és az ipari kender felé.</p>
+                <p>A Kenderlánc egyik alapítójaként az a célom, hogy fellendítsük a hazai ipari kender termesztését, segítsük  a mezőgazdaságot, és hosszú távon, közös együttműködésben erősítsük a növény útját.</p>
+                megismerkedni még jobban ezzel a haszonnövénnyel.</p>
+                <p>Rendezvényszervező szakmámnak köszönhetően sok hazai mezőgazdásszal, termelővel és szakemberrel tartom a kapcsolatot, illetve jómagam is elkezdtem mezőgazdasággal foglalkozni. Feleségemmel közösen épp egy 5 hektáros tanyára költözünk, beteljesítve ezzel végleges elköteleződésünket a természet és az ipari kender felé.</p>
+                <p>A Kenderlánc egyik alapítójaként az a célom, hogy fellendítsük a hazai ipari kender termesztését, segítsük  a mezőgazdaságot, és hosszú távon, közös együttműködésben erősítsük a növény útját.</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <h2>Vincze János (26)</h2>
+            <img src="" alt="">
+            <div class="member-description">
+                <p>Gyermekként (is) a természet volt a legjobb barátom. Nagyapám foglalkozása gulyás volt, nyaranta sok időt töltöttem vele a legelőkön és sokat tanított a természettel való összhangról. Felmenőim között csupa parasztemberek foglalnak helyet, így nem is volt soha kérdéses, hogy mivel szeretnék foglalkozni.</p>
+                <p>Pár évvel ezelőtt ez az álom kezdett egyre jobban kikristályosodni, ugyanis egy nagyszerű közösséghez csatlakozva, útjára indítottuk a Zengető fesztivált.  Már akkor mélyebben belevetettem magam az ipari kender rejtelmeibe és próbáltam a lehető legtöbb tudást magamhoz venni és megismerkedni még jobban ezzel a haszonnövénnyel.</p>
+                <p>Rendezvényszervező szakmámnak köszönhetően sok hazai mezőgazdásszal, termelővel és szakemberrel tartom a kapcsolatot, illetve jómagam is elkezdtem mezőgazdasággal foglalkozni. Feleségemmel közösen épp egy 5 hektáros tanyára költözünk, beteljesítve ezzel végleges elköteleződésünket a természet és az ipari kender felé.</p>
+                <p>A Kenderlánc egyik alapítójaként az a célom, hogy fellendítsük a hazai ipari kender termesztését, segítsük  a mezőgazdaságot, és hosszú távon, közös együttműködésben erősítsük a növény útját.</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <h2>Vincze János (26)</h2>
+            <img src="" alt="">
+            <div class="member-description">
+                <p>Gyermekként (is) a természet volt a legjobb barátom. Nagyapám foglalkozása gulyás volt, nyaranta sok időt töltöttem vele a legelőkön és sokat tanított a természettel való összhangról. Felmenőim között csupa parasztemberek foglalnak helyet, így nem is volt soha kérdéses, hogy mivel szeretnék foglalkozni.</p>
+                <p>Pár évvel ezelőtt ez az álom kezdett egyre jobban kikristályosodni, ugyanis egy nagyszerű közösséghez csatlakozva, útjára indítottuk a Zengető fesztivált.  Már akkor mélyebben belevetettem magam az ipari kender rejtelmeibe és próbáltam a lehető legtöbb tudást magamhoz venni és megismerkedni még jobban ezzel a haszonnövénnyel.</p>
+                <p>Rendezvényszervező szakmámnak köszönhetően sok hazai mezőgazdásszal, termelővel és szakemberrel tartom a kapcsolatot, illetve jómagam is elkezdtem mezőgazdasággal foglalkozni. Feleségemmel közösen épp egy 5 hektáros tanyára költözünk, beteljesítve ezzel végleges elköteleződésünket a természet és az ipari kender felé.</p>
+                <p>A Kenderlánc egyik alapítójaként az a célom, hogy fellendítsük a hazai ipari kender termesztését, segítsük  a mezőgazdaságot, és hosszú távon, közös együttműködésben erősítsük a növény útját.</p>
+                megismerkedni még jobban ezzel a haszonnövénnyel.</p>
+                <p>Rendezvényszervező szakmámnak köszönhetően sok hazai mezőgazdásszal, termelővel és szakemberrel tartom a kapcsolatot, illetve jómagam is elkezdtem mezőgazdasággal foglalkozni. Feleségemmel közösen épp egy 5 hektáros tanyára költözünk, beteljesítve ezzel végleges elköteleződésünket a természet és az ipari kender felé.</p>
+                <p>A Kenderlánc egyik alapítójaként az a célom, hogy fellendítsük a hazai ipari kender termesztését, segítsük  a mezőgazdaságot, és hosszú távon, közös együttműködésben erősítsük a növény útját.</p>
+            </div>
+        </div>
+    </div>
+    <div class="rolunk-team">
+        <h2>Cégünk (0)</h2>
+        <div class="member-description">
+            <p>Körbetekintve hazánkban szomorúan kellett tapasztalnunk, hogy nincsen megfelelő tudás és eszköz a piacon, hogy az ipari kenderben rejlő számtalan lehetőséget gyümölcsöztessük, ezért létrehoztuk cégünket melynek nem titkolt szándéka, hogy pótolja, azt a hiányzó láncszemet, amely képes összekötni ezt a sokrétű ágazatot. Együttműködéssel képesek leszünk a termelés, feldolgozás, termékelőállítás és értékesítés nehézségeivel megbírkózni, hogy az ipari kender ágazat felvirágozhasson!</p>
+        </div>
+        <p><strong>Hisz ha kendered van...</strong></p>
+    </div>
+</div>
+
+<?php get_footer();
