f4cb16934f2971665c3a622b7bb14fb7a91a1737
Bence Agócs <bence@harom.studio>
2021-10-20 11:54:37 +0200

    update single product add to cart with measurement acf

--- dist/woocommerce/single-product/add-to-cart/simple.php
+++ dist/woocommerce/single-product/add-to-cart/simple.php
@@ -54,9 +54,11 @@ if ( $product->is_in_stock() ) : ?>
 			do_action( 'woocommerce_after_add_to_cart_quantity' );
 			?>
 
-			<?php var_dump(the_field('mertekegyseg')); ?>
+			<span><?php the_field('mertekegyseg'); ?></span>
 
-			<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+			<div class="text-right">
+				<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+			</div>
 
 			<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 		</form>
