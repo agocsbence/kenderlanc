ffe0b7d7e120203f1dd0ed1feb4938c11d9a475b
Bence Agócs <bence@harom.studio>
2021-11-05 14:06:12 +0100

    update single page for variable description

--- dist/functions.php
+++ dist/functions.php
@@ -69,7 +69,7 @@ add_action( 'woocommerce_single_product_summary', 'woocommerce_show_product_imag
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs');
 //replace short descripiton
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 10);
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
 //remove price hook (added directly to thumbnails hook)
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
 //remove product meta
