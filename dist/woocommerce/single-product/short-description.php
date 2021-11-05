e09776f16ec34537f1ce68b8ead14bbdd93d1f93
Bence Agócs <bence@harom.studio>
2021-11-05 14:03:18 +0100

    update single page for variable description

--- dist/woocommerce/single-product/short-description.php
+++ dist/woocommerce/single-product/short-description.php
@@ -29,5 +29,6 @@ if ( ! $short_description ) {
 
 ?>
 <div class="woocommerce-product-details__short-description">
-	<?php echo $short_description; // WPCS: XSS ok. ?>
+	<?php // echo $short_description; // WPCS: XSS ok. ?>
+	<?php the_content(); ?>
 </div>
