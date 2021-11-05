b8c9d86a6caca6b698ae6d9c4eabd279d54b543c
Bence Agócs <bence@harom.studio>
2021-11-05 14:28:57 +0100

    update single page for variable description

--- src/page-lexikon.php
+++ src/page-lexikon.php
@@ -109,6 +109,306 @@ get_header(); ?>
                 wp_reset_postdata();?>
                 <?php endif; ?>
             </div>
+            <div class="letter">
+                <h2 id="g">G</h2>
+                
+                <?php $query = new WP_Query( 'cat=34' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="h">H</h2>
+                
+                <?php $query = new WP_Query( 'cat=35' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="i">I</h2>
+                
+                <?php $query = new WP_Query( 'cat=36' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="j">J</h2>
+                
+                <?php $query = new WP_Query( 'cat=37' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="k">K</h2>
+                
+                <?php $query = new WP_Query( 'cat=38' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="l">L</h2>
+                
+                <?php $query = new WP_Query( 'cat=39' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="m">M</h2>
+                
+                <?php $query = new WP_Query( 'cat=40' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="n">N</h2>
+                
+                <?php $query = new WP_Query( 'cat=41' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="o">O</h2>
+                
+                <?php $query = new WP_Query( 'cat=42' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="p">P</h2>
+                
+                <?php $query = new WP_Query( 'cat=43' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="q">Q</h2>
+                
+                <?php $query = new WP_Query( 'cat=44' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="r">R</h2>
+                
+                <?php $query = new WP_Query( 'cat=45' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="s">S</h2>
+                
+                <?php $query = new WP_Query( 'cat=46' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="t">T</h2>
+                
+                <?php $query = new WP_Query( 'cat=47' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="u">U</h2>
+                
+                <?php $query = new WP_Query( 'cat=93' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="v">V</h2>
+                
+                <?php $query = new WP_Query( 'cat=48' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="w">W</h2>
+                
+                <?php $query = new WP_Query( 'cat=49' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="x">X</h2>
+                
+                <?php $query = new WP_Query( 'cat=1' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="y">Y</h2>
+                
+                <?php $query = new WP_Query( 'cat=51' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
+            <div class="letter">
+                <h2 id="z">Z</h2>
+                
+                <?php $query = new WP_Query( 'cat=52' ); ?>
+                <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>                
+                    <div class="lexikon-article">
+                        <h3><?php the_title(); ?></h3>
+                        <div class="text-block">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; 
+                wp_reset_postdata();?>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </div>
