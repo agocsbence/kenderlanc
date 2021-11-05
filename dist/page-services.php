fbda6f23826063c44993acad4183097ac61a4b95
Bence Agócs <bence@harom.studio>
2021-11-05 11:48:09 +0100

    update services page template

--- dist/page-services.php
+++ dist/page-services.php
@@ -36,6 +36,59 @@ get_header(); ?>
             </div>
         </a>
     </div>
+    <div class="grid grid--home-tiles">
+        <!-- loop here -->
+        <?php
+        
+        $loop = new WP_Query( array( 'post_type' => 'szolgaltatas', 'posts_per_page' => 1 ) );
+        
+        if ( $loop->have_posts() ) :
+            while ( $loop->have_posts() ) : $loop->the_post();
+            $id = get_the_ID();
+            $image = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'full', false ); ?>
+
+                <a href="<?php echo get_permalink(); ?>" class="grid-tile big-tile title-top bg--image" style="background-image: url('<?php echo $image[0]; ?>');">
+                    <div class="content">
+                        <div class="content-inside">
+                            <h2><?php the_title(); ?></h2>
+                        </div>
+                    </div>
+                </a>
+
+            <?php 
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+
+        <!-- end of loop -->
+
+        <!-- loop here for the smaller tiles -->
+        <?php
+        
+        $loop = new WP_Query( array( 'post_type' => 'szolgaltatas', 'offset' => 1 ) );
+        
+        if ( $loop->have_posts() ) :
+            while ( $loop->have_posts() ) : $loop->the_post();
+            $id = get_the_ID();
+            $image = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'full', false ); ?>
+
+                <a href="<?php echo get_permalink(); ?>" class="grid-tile bg--color">
+                    <div class="content">
+                        <div class="content-inside">
+                            <h2><?php the_title(); ?></h2>
+                        </div>
+                    </div>
+                </a>
+
+            <?php 
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+
+        <!-- end of loop -->
+    </div>
 </div>
 
 <?php get_footer();
